e2a70c722ffcb1f0fb528e1e82fcdd80ee43d9ab
Ash Midgley <ashleymidgley@gmail.com>
2022-06-24 11:30:22 +1200

    Add support for additional child elements.

--- dist/components/SVGMap.js
+++ dist/components/SVGMap.js
@@ -11,6 +11,87 @@ var _propTypes = _interopRequireDefault(require("prop-types"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+const getChild = (element, onPathMouseOver, onPathMouseMove, onPathMouseOut) => {
+  switch (element.type) {
+    case "path":
+      return /*#__PURE__*/_react.default.createElement("path", {
+        key: element.id,
+        id: element.id,
+        name: element.name,
+        d: element.d,
+        "aria-label": element.name,
+        onMouseOver: onPathMouseOver,
+        onMouseMove: onPathMouseMove,
+        onMouseOut: onPathMouseOut,
+        style: element.style ? element.style : {}
+      });
+
+    case "polygon":
+      return /*#__PURE__*/_react.default.createElement("polygon", {
+        key: element.id,
+        id: element.id,
+        name: element.name,
+        points: element.points,
+        "aria-label": element.name,
+        onMouseOver: onPathMouseOver,
+        onMouseMove: onPathMouseMove,
+        onMouseOut: onPathMouseOut,
+        style: element.style ? element.style : {}
+      });
+
+    case "circle":
+      return /*#__PURE__*/_react.default.createElement("circle", {
+        key: element.id,
+        id: element.id,
+        name: element.name,
+        cx: element.cx,
+        cy: element.cy,
+        r: element.r,
+        style: element.style ? element.style : {}
+      });
+
+    case "rect":
+      return /*#__PURE__*/_react.default.createElement("rect", {
+        key: element.id,
+        id: element.id,
+        name: element.name,
+        x: element.x,
+        y: element.y,
+        width: element.width,
+        height: element.height,
+        transform: element.transform,
+        style: element.style ? element.style : {}
+      });
+
+    case "image":
+      return /*#__PURE__*/_react.default.createElement("image", {
+        key: element.id,
+        id: element.id,
+        name: element.name,
+        width: element.width,
+        height: element.height,
+        transform: element.transform,
+        xlinkHref: element.xlinkHref,
+        "clip-path": element.clipPath
+      });
+
+    case "defs":
+      return /*#__PURE__*/_react.default.createElement("defs", {
+        key: element.id,
+        id: element.id,
+        name: element.name
+      }, /*#__PURE__*/_react.default.createElement("clipPath", {
+        id: element.clipPathId
+      }, /*#__PURE__*/_react.default.createElement("rect", {
+        width: element.width,
+        height: element.height
+      })));
+
+    default:
+      throw Error("Invalid SVG child type.");
+  }
+};
+
 const SVGMap = _ref => {
   let {
     map,
@@ -21,35 +102,11 @@ const SVGMap = _ref => {
   } = _ref;
   return /*#__PURE__*/_react.default.createElement("svg", {
     xmlns: "http://www.w3.org/2000/svg",
+    xmlnsXlink: "http://www.w3.org/1999/xlink",
     viewBox: map.viewBox,
     "aria-label": map.label,
     style: mapStyle
-  }, map.elements.map(element => {
-    switch (element.type) {
-      case "circle":
-        return /*#__PURE__*/_react.default.createElement("circle", {
-          id: element.id,
-          name: element.name,
-          cx: element.cx,
-          cy: element.cy,
-          r: element.r,
-          style: element.style ? element.style : {}
-        });
-
-      default:
-        return /*#__PURE__*/_react.default.createElement("path", {
-          key: element.id,
-          id: element.id,
-          name: element.name,
-          d: element.d,
-          "aria-label": element.name,
-          onMouseOver: onPathMouseOver,
-          onMouseMove: onPathMouseMove,
-          onMouseOut: onPathMouseOut,
-          style: element.style ? element.style : {}
-        });
-    }
-  }));
+  }, map.elements.map(element => getChild(element, onPathMouseOver, onPathMouseMove, onPathMouseOut)));
 };
 
 SVGMap.propTypes = {
@@ -61,9 +118,18 @@ SVGMap.propTypes = {
       id: _propTypes.default.string.isRequired,
       name: _propTypes.default.string.isRequired,
       d: _propTypes.default.string,
+      points: _propTypes.default.string,
+      x: _propTypes.default.string,
+      y: _propTypes.default.string,
+      width: _propTypes.default.string,
+      height: _propTypes.default.string,
       cx: _propTypes.default.string,
       cy: _propTypes.default.string,
       r: _propTypes.default.string,
+      transform: _propTypes.default.string,
+      xlinkHref: _propTypes.default.string,
+      clipPath: _propTypes.default.string,
+      clipPathId: _propTypes.default.string,
       style: _propTypes.default.object
     }))
   }).isRequired,
--- src/lib/components/SVGMap.jsx
+++ src/lib/components/SVGMap.jsx
@@ -1,6 +1,92 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const getChild = (element, onPathMouseOver, onPathMouseMove, onPathMouseOut) => {
+  switch (element.type) {
+    case "path":
+      return (
+        <path
+          key={element.id}
+          id={element.id}
+          name={element.name}
+          d={element.d}
+          aria-label={element.name}
+          onMouseOver={onPathMouseOver}
+          onMouseMove={onPathMouseMove}
+          onMouseOut={onPathMouseOut}
+          style={element.style ? element.style : {}}
+        />
+      );
+    case "polygon":
+      return (
+        <polygon
+          key={element.id}
+          id={element.id}
+          name={element.name}
+          points={element.points}
+          aria-label={element.name}
+          onMouseOver={onPathMouseOver}
+          onMouseMove={onPathMouseMove}
+          onMouseOut={onPathMouseOut}
+          style={element.style ? element.style : {}}
+        />
+      );
+    case "circle":
+      return (
+        <circle
+          key={element.id}
+          id={element.id}
+          name={element.name}
+          cx={element.cx}
+          cy={element.cy}
+          r={element.r}
+          style={element.style ? element.style : {}}
+        />
+      );
+    case "rect":
+      return (
+        <rect
+          key={element.id}
+          id={element.id}
+          name={element.name}
+          x={element.x}
+          y={element.y}
+          width={element.width}
+          height={element.height}
+          transform={element.transform}
+          style={element.style ? element.style : {}}
+        />
+      );
+    case "image":
+      return (
+        <image
+          key={element.id}
+          id={element.id}
+          name={element.name}
+          width={element.width}
+          height={element.height}
+          transform={element.transform}
+          xlinkHref={element.xlinkHref}
+          clip-path={element.clipPath}
+        />
+      );
+    case "defs":
+      return (
+        <defs
+          key={element.id}
+          id={element.id}
+          name={element.name}
+        >
+            <clipPath id={element.clipPathId}>
+                <rect width={element.width} height={element.height}/>
+            </clipPath>
+        </defs>
+      );
+    default:
+     throw Error("Invalid SVG child type.");
+  }
+};
+
 const SVGMap = ({
   map,
   mapStyle,
@@ -10,39 +96,12 @@ const SVGMap = ({
 }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
     viewBox={map.viewBox}
     aria-label={map.label}
     style={mapStyle}
   >
-    {map.elements.map((element) => {
-      switch (element.type) {
-        case "circle":
-          return (
-            <circle
-              id={element.id}
-              name={element.name}
-              cx={element.cx}
-              cy={element.cy}
-              r={element.r}
-              style={element.style ? element.style : {}}
-            />
-          );
-        default:
-          return (
-            <path
-              key={element.id}
-              id={element.id}
-              name={element.name}
-              d={element.d}
-              aria-label={element.name}
-              onMouseOver={onPathMouseOver}
-              onMouseMove={onPathMouseMove}
-              onMouseOut={onPathMouseOut}
-              style={element.style ? element.style : {}}
-            />
-          );
-      }
-    })}
+    {map.elements.map((element) => getChild(element, onPathMouseOver, onPathMouseMove, onPathMouseOut))}
   </svg>
 );
 
@@ -56,9 +115,18 @@ SVGMap.propTypes = {
         id: PropTypes.string.isRequired,
         name: PropTypes.string.isRequired,
         d: PropTypes.string,
+        points: PropTypes.string,
+        x: PropTypes.string,
+        y:PropTypes.string,
+        width: PropTypes.string,
+        height: PropTypes.string,
         cx: PropTypes.string,
 				cy: PropTypes.string,
 				r: PropTypes.string,
+        transform: PropTypes.string,
+        xlinkHref: PropTypes.string,
+        clipPath: PropTypes.string,
+        clipPathId: PropTypes.string,
         style: PropTypes.object,
       })
     ),
